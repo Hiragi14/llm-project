2cc6f0fd9cb2251f98d66d7ae399463b4bff55a6
kawai <kawai.syusei@gmail.com>
2025-01-29 20:49:03 +0900

    build for github

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kawai/llm-project/' // ベースURLを設定
+  base: 's/llm-project/' // ベースURLを設定
 });
